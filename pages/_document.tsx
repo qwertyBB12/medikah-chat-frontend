84c96729c0af79cf36d1890968574520f95276e1
Hector H. Lopez <hhlopez@gmail.com>
2026-02-02 01:37:19 -0600

    Add favicons for all device sizes and web manifest
    16, 32, 48 (ICO), 96, 180 (apple-touch), 192, 512px.
    Includes site.webmanifest and theme-color meta tag.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,15 @@ import { mulish, ibmPlexMono } from '../lib/fonts';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon-96x96.png" type="image/png" sizes="96x96" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#1B2A41" />
+      </Head>
       <body className={`${mulish.variable} ${ibmPlexMono.variable} font-body`}>
         <Main />
         <NextScript />
