0fe12aec08afd4d36d643b004fc0f9ffae967a5f
Hector H. Lopez <hhlopez@gmail.com>
2026-03-07 14:15:45 -0600

    chore(01-01): delete design-preview pages and remove Sanity dependency
    - Delete 4 design-preview page files (SEC-01 through SEC-04)
    - Delete unused lib/sanity.ts client module
    - Remove @sanity/client from package.json dependencies
    - Remove Sanity domains from CSP connect-src in next.config.js

--- lib/sanity.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@sanity/client';
-
-export const sanityClient = createClient({
-  projectId: 'fo6n8ceo',
-  dataset: 'production',
-  apiVersion: '2024-01-01',
-  useCdn: true,
-});
